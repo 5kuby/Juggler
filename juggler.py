435c0c50e4b8f9d3dcf673e83cc09541912e0327
Davide "5KUBY" Pala <skullb0y@protonmail.ch>
2017-05-28 12:56:33 +0200

    juggler_1_0_9
    Added years option

--- juggler.py
+++ juggler.py
@@ -1,27 +1,3 @@
-"""
-
-
-  ___                   _           
-  |_  |                 | |          
-    | |_   _  __ _  __ _| | ___ _ __ 
-    | | | | |/ _` |/ _` | |/ _ \ '__|
-/\__/ / |_| | (_| | (_| | |  __/ |   
-\____/ \__,_|\__, |\__, |_|\___|_|   
-              __/ | __/ |            
-             |___/ |___/             
-
-                            By
-                     _____ _          _           
-                    |  ___| |        | |          
-                    |___ \| | ___   _| |__  _   _ 
-                        \ \ |/ / | | | '_ \| | | |
-                    /\__/ /   <| |_| | |_) | |_| |
-                    \____/|_|\_\\__,_|_.__/ \__, |
-                                            __/ |
-                                            |___/ 
-this script modify words to generate valid password with some common pattern
-
-"""
 def dictionary(input_character):
     mapping={'a':'a@4A', 'b':'bB8', 'c':'cC', 'd':'dD','e':'e3€&E', 'f':'fF', 'g':'gG', 'h':'hH', 'i':'i1!I', 'l':'l|£L', 'm':'mM', 'n':'nN', 'p':'pP', 'q':'qQ', 'r':'rR', 's':'s5$S', 't':'t7T', 'u':'uU', 'v':'vV', 'z':'zZ', 'o':'o0O'}
     return mapping.get(input_character,input_character)
@@ -34,12 +10,82 @@ def generate(base):
                 yield character+rest
 
 def passgen(base):
+    passlist=[]
     for generated in generate(base):
         generated = generated + "\n"
-        outfilename.write(generated)
+        passlist.append(generated)
+    for record in passlist:
+        outfilename.write(record)
+
+def years(base):
+    passlist=[]
+    year_start = int(input("Years range start at: "))
+    year_stop = int(input("Years range stop at: "))
+    for generated in generate(base):
+        for year in range(year_start,year_stop+1):
+            modified_record = generated+str(year)
+            passlist.append(modified_record)
+            year = year % 100
+            modified_record = generated+str('%02d'%year)
+            passlist.append(modified_record)
+    for record in passlist:
+        record = record + "\n"
+        outfilename.write(record)
+
+def pointedyears(base):
+    passlist=[]
+    year_start = int(input("Years range start at: "))
+    year_stop = int(input("Years range stop at: "))
+    divisor =['.','-','_']
+    for generated in generate(base):
+        for year in range(year_start,year_stop+1):
+            modified_record = generated+str(year)
+            passlist.append(modified_record)
+            for div in divisor:
+                modified_record = generated+div+str(year)
+                passlist.append(modified_record)
+            year = year % 100
+            modified_record = generated+str('%02d'%year)
+            passlist.append(modified_record)
+            for div in divisor:
+                modified_record = generated+div+str('%02d'%year)
+                passlist.append(modified_record)
+    for record in passlist:
+        record = record + "\n"
+        outfilename.write(record)
+        
+options = {'s':passgen,'y':years,'p':pointedyears,'S':'symbols','A':'all'}     
+def main():
+    select = str(input("Select a modifier: \ns for substitution\ny for years append\np for pointed years append\n"))
+    return select        
+
 ########### SCRIPT MAIN SECTION ##########
+title ="""
+   ___                   _           
+  |_  |                 | |          
+    | |_   _  __ _  __ _| | ___ _ __ 
+    | | | | |/ _` |/ _` | |/ _ \ '__|
+/\__/ / |_| | (_| | (_| | |  __/ |   
+\____/ \__,_|\__, |\__, |_|\___|_|   
+              __/ | __/ |            
+             |___/ |___/             
+                            By
+                     _____ _          _           
+                    |  ___| |        | |          
+                    |___ \| | ___   _| |__  _   _ 
+                        \ \ |/ / | | | '_ \| | | |
+                    /\__/ /   <| |_| | |_) | |_| |
+                    \____/|_|\_\\__,_|_.__/ \__, |
+                                            __/ |
+                                           |___/ 
+\nthis script modify words to generate valid password with some common pattern:\n\n\n
+Subtitution: Do a substitution of every character in the word with a similar number or symbols, this option also do lowercase to uppercase modification and vice versa
+Year: Same as substitution with the add of a year ate the end of the word. User can specify the years range. 
+"""
+print (title)
 Input_Name = input("Type a word ")
 filename = input("Type an output file name ")
-outfilename = open(filename,"w")
-passgen(Input_Name)
+User_choice = main()
+outfilename = open(filename,"a")
+options[User_choice](Input_Name)
 outfilename.close()
